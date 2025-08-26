65b6e7d9670438d164e8a1d071a3f19bfa822e6b
egroehs <eduardagroehs@gmail.com>
2025-08-26 14:39:23 -0300

    feat: how to cite us

--- src/pages/howToCite/howToCite.tsx
+++ src/pages/howToCite/howToCite.tsx
@@ -1,3 +1,25 @@
+import './howToCite.css';
+
 export const HowToCite = () => {
-  return <section>how to cite</section>;
+  return (
+    <section className="how-to-cite-us-section">
+      
+        <p>
+          If you use ASI-PRED in your research, please cite the software as
+          follows: 
+        </p>
+        <div>
+          <p>
+            ASI-PRED – Amino Acid Substitution Index Predictor. </p>
+            <p>Available
+            at: https://asi-pred-frontend.onrender.com </p>
+            <p>DOI:
+            https://doi.org/10.5281/zenodo.16950195 </p>
+            <p>Accessed: August 2025 (e.g. ). </p>
+          </div>
+          <p> A manuscript describing ASI-PRED is currently in preparation and will be
+          available for formal citation in the near future.</p>
+        
+    </section>
+  );
 };
