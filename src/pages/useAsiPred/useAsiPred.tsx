e60b1ace58382e46fd7d0859134be37c4e2457ce
egroehs <eduardagroehs@gmail.com>
2025-08-28 14:59:31 -0300

    fix scroll

--- src/pages/useAsiPred/useAsiPred.tsx
+++ src/pages/useAsiPred/useAsiPred.tsx
@@ -24,166 +24,167 @@ export type CombinationWeightType = [
 ];
 
 export const UseAsiPred = () => {
-    const [aa1, setAa1] = useState('');
-    const [aa2, setAa2] = useState("");
-    const [id, setId] = useState('');
-    const [aa1Active, setAa1Active] = useState(false);
-    const [aa2Active, setAa2Active] = useState(false);
-    const [combinations, setCombinations] = useState<[string, string, string, string][]>([]);
-    const [disulfideBondWeight, setDisulfideBondWeight] = useState("");
-    const [disulfideBond, setDisulfideBond] = useState(false);
-    const [granthamWeight, setGranthamWeight] = useState('0.2');
-    const [sneathWeight, setSneathWeight] = useState("0.2");
-    const [epsteinsWeight, setEpsteinsWeight] = useState("0.2");
-    const [miyataWeight, setMiyataWeight] = useState("0.2");
-    const [
-      experimentalExchangeabilityWeight,
-      setExperimentalExchangeabilityWeight,
-    ] = useState("0.2");
+  const [aa1, setAa1] = useState("");
+  const [aa2, setAa2] = useState("");
+  const [id, setId] = useState("");
+  const [aa1Active, setAa1Active] = useState(false);
+  const [aa2Active, setAa2Active] = useState(false);
+  const [combinations, setCombinations] = useState<
+    [string, string, string, string][]
+  >([]);
+  const [disulfideBondWeight, setDisulfideBondWeight] = useState("");
+  const [disulfideBond, setDisulfideBond] = useState(false);
+  const [granthamWeight, setGranthamWeight] = useState("0.2");
+  const [sneathWeight, setSneathWeight] = useState("0.2");
+  const [epsteinsWeight, setEpsteinsWeight] = useState("0.2");
+  const [miyataWeight, setMiyataWeight] = useState("0.2");
+  const [
+    experimentalExchangeabilityWeight,
+    setExperimentalExchangeabilityWeight,
+  ] = useState("0.2");
 
-    const [showWeights, setShowWeights] = useState(false);
-    const [hasAnyDisulfideBond, setHasAnyDisulfideBond] = useState(false);
-    const [submitted, setSubmitted] = useState(false);
-    const [combinationsWeight, setCombinationsWeight] =
-      useState<CombinationWeightType>(["", "", "", "", "", undefined]);
+  const [showWeights, setShowWeights] = useState(false);
+  const [hasAnyDisulfideBond, setHasAnyDisulfideBond] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
+  const [combinationsWeight, setCombinationsWeight] =
+    useState<CombinationWeightType>(["", "", "", "", "", undefined]);
 
-    useEffect(() => {
-      const hasBond = combinations.some(
-        (combination) => combination[3] === "true"
-      );
-      setHasAnyDisulfideBond(hasBond);
-    }, [combinations]);
+  useEffect(() => {
+    const hasBond = combinations.some(
+      (combination) => combination[3] === "true"
+    );
+    setHasAnyDisulfideBond(hasBond);
+  }, [combinations]);
 
+  const totalWeights = useMemo(() => {
+    return (
+      parseFloat(granthamWeight) +
+      parseFloat(sneathWeight) +
+      parseFloat(epsteinsWeight) +
+      parseFloat(miyataWeight) +
+      parseFloat(experimentalExchangeabilityWeight)
+    );
+  }, [
+    granthamWeight,
+    sneathWeight,
+    epsteinsWeight,
+    miyataWeight,
+    experimentalExchangeabilityWeight,
+  ]);
 
-    const totalWeights = useMemo(() => {
-      return (
-        parseFloat(granthamWeight) +
-        parseFloat(sneathWeight) +
-        parseFloat(epsteinsWeight) +
-        parseFloat(miyataWeight) +
-        parseFloat(experimentalExchangeabilityWeight)
-      );
-    }, [
-      granthamWeight,
-      sneathWeight,
-      epsteinsWeight,
-      miyataWeight,
-      experimentalExchangeabilityWeight,
-    ]);
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
-    
-    useEffect(() => {
-      if (submitted) {
-        navigate("/results", { state: { combinations, combinationsWeight } });
-        setSubmitted(false);
-      }
-    }, [combinations, combinationsWeight, submitted]);
+  useEffect(() => {
+    if (submitted) {
+      navigate("/results", { state: { combinations, combinationsWeight } });
+      setSubmitted(false);
+    }
+  }, [combinations, combinationsWeight, submitted]);
 
-      return (
-        <div className="use-asi-pred-page">
-          <Input id={id} handleInputChange={handleInputChange(setId)} />
-          <Aa1Selection
-            aa1={aa1}
-            aa1Active={aa1Active}
-            setAa1={setAa1}
-            setAa1Active={setAa1Active}
+  return (
+    <div className="use-asi-pred-page">
+      <Input id={id} handleInputChange={handleInputChange(setId)} />
+      <Aa1Selection
+        aa1={aa1}
+        aa1Active={aa1Active}
+        setAa1={setAa1}
+        setAa1Active={setAa1Active}
+      />
+      <Aa2Selection
+        aa2={aa2}
+        aa2Active={aa2Active}
+        setAa2={setAa2}
+        setAa2Active={setAa2Active}
+      />
+      <ButtonSection
+        aa1={aa1}
+        aa2={aa2}
+        id={id}
+        setAa1={setAa1}
+        setAa1Active={setAa1Active}
+        setAa2={setAa2}
+        setAa2Active={setAa2Active}
+        setCombinations={setCombinations}
+        setId={setId}
+        setDisulfideBond={setDisulfideBond}
+        totalWeights={totalWeights}
+        disulfideBond={disulfideBond}
+        onContinue={() => {
+          handleAddCombination({
+            aa1,
+            aa2,
+            id,
+            disulfideBond,
+            setAa1,
+            setAa2,
+            setAa1Active,
+            setAa2Active,
+            setId,
+            setCombinations,
+          });
+          setShowWeights(true);
+          window.scrollTo({ top: 1000, behavior: "smooth" });
+        }}
+      />
+      <div className="selected-combinations">
+        {combinations.map((combination, index) => (
+          <p key={index}>
+            Selected combination: {combination[0]} (
+            {getAminoacidName(combination[1])}
+            <GoArrowRight />
+            {getAminoacidName(combination[2])})
+            {combination[3] === "true" && (
+              <span> with disulfide bond disruption.</span>
+            )}
+          </p>
+        ))}
+      </div>
+
+      {showWeights && (
+        <>
+          <SelectIndexWeight
+            handleWeightChange={handleWeightChange}
+            setExperimentalExchangeabilityWeight={
+              setExperimentalExchangeabilityWeight
+            }
+            setGranthamWeight={setGranthamWeight}
+            setSneathWeight={setSneathWeight}
+            setEpsteinsWeight={setEpsteinsWeight}
+            setMiyataWeight={setMiyataWeight}
+            setDisulfideBondWeight={setDisulfideBondWeight}
+            hasAnyDisulfideBond={hasAnyDisulfideBond}
           />
-          <Aa2Selection
-            aa2={aa2}
-            aa2Active={aa2Active}
-            setAa2={setAa2}
-            setAa2Active={setAa2Active}
-          />
-          <ButtonSection
-            aa1={aa1}
-            aa2={aa2}
-            id={id}
-            setAa1={setAa1}
-            setAa1Active={setAa1Active}
-            setAa2={setAa2}
-            setAa2Active={setAa2Active}
-            setCombinations={setCombinations}
-            setId={setId}
-            setDisulfideBond={setDisulfideBond}
-            totalWeights={totalWeights}
-            disulfideBond={disulfideBond}
-            onContinue={() => {
-              handleAddCombination({
-                aa1,
-                aa2,
-                id,
-                disulfideBond,
-                setAa1,
-                setAa2,
-                setAa1Active,
-                setAa2Active,
-                setId,
-                setCombinations,
-              });
-              setShowWeights(true);
-            }}
-          />
-          <div className="selected-combinations">
-            {combinations.map((combination, index) => (
-              <p key={index}>
-                Selected combination: {combination[0]} (
-                {getAminoacidName(combination[1])}
-                <GoArrowRight />
-                {getAminoacidName(combination[2])})
-                {combination[3] === "true" && (
-                  <span> with disulfide bond disruption.</span>
-                )}
-              </p>
-            ))}
+
+          <div className="submit-button-section">
+            <button
+              aria-label="Submit button"
+              className="submit-button"
+              onClick={() => {
+                handleSubmit({
+                  combinations,
+                  totalWeights,
+                  granthamWeight,
+                  sneathWeight,
+                  epsteinsWeight,
+                  miyataWeight,
+                  experimentalExchangeabilityWeight,
+                  disulfideBondWeight,
+                  setCombinationsWeight,
+                  setSubmitted,
+                });
+              }}
+            >
+              Submit
+            </button>
+            <a
+              href="terms-and-conditions"
+              aria-label="Link to terms and conditions page."
+            >
+              Terms and Conditions
+            </a>
           </div>
-
-          {showWeights && (
-            <>
-              <SelectIndexWeight
-                handleWeightChange={handleWeightChange}
-                setExperimentalExchangeabilityWeight={
-                  setExperimentalExchangeabilityWeight
-                }
-                setGranthamWeight={setGranthamWeight}
-                setSneathWeight={setSneathWeight}
-                setEpsteinsWeight={setEpsteinsWeight}
-                setMiyataWeight={setMiyataWeight}
-                setDisulfideBondWeight={setDisulfideBondWeight}
-                hasAnyDisulfideBond={hasAnyDisulfideBond}
-              />
-            
-            <div className="submit-button-section">
-              <button
-                aria-label="Submit button"
-                className="submit-button"
-                onClick={() =>  {
-                  handleSubmit({
-                    combinations,
-                    totalWeights,
-                    granthamWeight,
-                    sneathWeight,
-                    epsteinsWeight,
-                    miyataWeight,
-                    experimentalExchangeabilityWeight,
-                    disulfideBondWeight,
-                    setCombinationsWeight,
-                    setSubmitted,
-                  });
-                }}
-              >
-                Submit
-              </button>
-                <a 
-                  href="terms-and-conditions"
-                  aria-label="Link to terms and conditions page."
-                >Terms and Conditions</a>
-            </div>
-
-            </>
-          )}
-          
-
-        </div>
-      );
-}
+        </>
+      )}
+    </div>
+  );
+};
